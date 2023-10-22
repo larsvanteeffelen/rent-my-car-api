e14322286b6bdacab64d913b66c7535c0bfd2ccf
Daan van Geloven <kingdaano@gmail.com>
2023-10-22 17:20:23 +0200

    Added base for test

--- src/test/kotlin/nl/avans/ApplicationTest.kt
+++ src/test/kotlin/nl/avans/ApplicationTest.kt
@@ -9,10 +9,12 @@ import io.ktor.http.cio.*
 import io.ktor.server.response.*
 import io.ktor.server.testing.*
 import io.ktor.server.util.*
+import nl.avans.dao.CarDAO
 import nl.avans.dao.UserDAO
 import nl.avans.dto.User
 import kotlin.test.*
 import nl.avans.plugins.*
+import nl.avans.routes.cars.configureCarRouting
 import nl.avans.routes.users.configureUserRouting
 import org.h2.util.JdbcUtils.serializer
 import java.sql.Connection
@@ -52,10 +54,118 @@ class ApplicationTest {
                 // Verify the response content (user ID)
                 // If the user table is empty then will pass this test with every run there is one user added by the registration test.
                 val id = response.content
-                assertEquals("10", id)
+
+                // Check that retrieving the user returns status code 200 OK
+                handleRequest(HttpMethod.Get, "/user/$id"){
+                    addHeader("Content-Type", ContentType.Application.Json.toString())
+                }.apply {  assertEquals(HttpStatusCode.OK, response.status()) }
+
             }
         }
     }
+
+    @Test
+    fun testUserLogin() {
+        val connection = connectToPostgres(embedded = true) // Use the embedded database
+
+        // Initialize the UserDAO with the database connection
+        val userDAO = connection?.let { UserDAO(it) }
+
+        withTestApplication({
+            configureSecurity()
+            configureSerialization()
+            if (userDAO != null) {
+                configureUserRouting(userDAO)
+            }
+        }) {
+            handleRequest(HttpMethod.Get, "/user/1") {
+                addHeader("Content-Type", ContentType.Application.Json.toString())
+            }.apply {
+                // Verify the response status code
+                assertEquals(HttpStatusCode.OK, response.status())
+
+                // Verify the response content (user ID)
+                // If the user table is empty then will pass this test with every run there is one user added by the registration test.
+                TODO("Add Assertions for test")
+                print(response.content)
+
+
+
+            }
+        }
+    }
+
+    @Test
+    fun testAvailableCars() {
+        val connection = connectToPostgres(embedded = true) // Use the embedded database
+
+        // Initialize the CarDAO with the database connection
+        val carDAO = connection?.let { CarDAO(it) }
+
+        withTestApplication({
+            configureSecurity()
+            configureSerialization()
+            if (carDAO != null) {
+                configureCarRouting(carDAO)
+            }
+        }) {
+            handleRequest(HttpMethod.Get, "/cars/all") {
+                addHeader("Content-Type", ContentType.Application.Json.toString())
+            }.apply {
+                // Verify the response status code
+                assertEquals(HttpStatusCode.OK, response.status())
+
+                // Verify the response content (user ID)
+                // If the user table is empty then will pass this test with every run there is one user added by the registration test.
+                TODO("Add Assertions for car available test")
+                print(response.content)
+
+
+
+            }
+        }
+    }
+
+    @Test
+    fun testCarDetails() {
+        val connection = connectToPostgres(embedded = true) // Use the embedded database
+
+        // Initialize the CarDAO with the database connection
+        val carDAO = connection?.let { CarDAO(it) }
+
+        withTestApplication({
+            configureSecurity()
+            configureSerialization()
+            if (carDAO != null) {
+                configureCarRouting(carDAO)
+            }
+        }) {
+            handleRequest(HttpMethod.Get, "/cars/1") {
+                addHeader("Content-Type", ContentType.Application.Json.toString())
+            }.apply {
+                // Verify the response status code
+                assertEquals(HttpStatusCode.OK, response.status())
+
+                // Verify the response content (user ID)
+                // If the user table is empty then will pass this test with every run there is one user added by the registration test.
+                TODO("Add Assertions for car detail test")
+                print(response.content)
+
+
+
+            }
+        }
+    }
+
+//    Test should fail
+    @Test
+    fun cancelBooking() {
+
+    }
+
+
+
+
 // Connecton with the database postgresSQL in DOcker environment.
     private fun connectToPostgres(embedded: Boolean): Connection? {
         Class.forName("org.postgresql.Driver")
